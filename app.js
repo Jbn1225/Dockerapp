e657c2cb0e6777dba46e62a686570c3844566260
jbn1225 <mariambiju375@gmail.com>
2024-11-21 21:08:04 +0000

    changed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 80;
 
 app.get("/", (req, res) => {
-  res.send("TERRAFORM + ANSIBLE + AWS +AZURE DEVOPS - ITS ALL FUN!");
+  res.send("Jebin,Boby,Ansil,Aswin");
 });
 
 app.listen(PORT, () => {
